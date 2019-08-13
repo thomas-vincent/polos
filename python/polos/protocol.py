56f8260b0e16fc18008ed35da85029fa35f2a67c
tvvincent <20100thomas@gmail.com>
2019-08-12 23:13:20 -0400

    Add pulse width modulation protocol to transmit values via a single analog channel

---
+++ python/polos/protocol.py
@@ -0,0 +1,387 @@
+import time
+from threading import Thread
+import logging
+import numpy as np
+
+logger = logging.getLogger('polos')
+
+class DppDecodeError(Exception): pass
+
+class DiscretePwmProtocol:
+    """
+    DiscretePwmProtocol (Dpp) transmits float values using only pulses, 
+    in a (slow) fail-safe manner.
+    Encoding is done by pulse width modulation. The sending interface
+    is expected to be a logic on/off emitter. 
+
+    The encoding depends on the sampling rate of the recieving interface, so
+    that sent pulses are wide enough to be caught. Any constant NB_SAMPLES...
+    stands for a multiple of the recording pace in the receiving end.
+    
+    The Dpp segment consists of:
+    SEP.            : a pulse of width NB_SAMPLES_SEPARATOR
+    FIXED_OPENING   : a pulse of width NB_SAMPLES_DELIMITER
+    SEP.            : a pulse of width NB_SAMPLES_SEPARATOR
+    PRECISION_BITS  : a binary encoding of the number of decimal digits in the output
+                      value. TODO
+    SEP.            : a pulse of width NB_SAMPLES_SEPARATOR
+    VALUE_ENCODING  : a binary encoding of the output value (without decimals)
+    SEP.            : a pulse of width NB_SAMPLES_SEPARATOR
+    FIXED_CLOSING   : a pulse of width NB_SAMPLES_DELIMITER (same as FIXED_OPENING)
+    SEP.            : a pulse of width NB_SAMPLES_SEPARATOR
+
+    A "binary encoding" segment is a pulse representation of a binary sequence,
+    where:
+        The sent pulse for a bit of value 1 spans NB_SAMPLES_BIT1 samples 
+        The sent pulse for a bit of value 0 spans NB_SAMPLES_BIT0 samples 
+        The constants NB_SAMPLES_BIT1 and NB_SAMPLES_BIT0 are set so that decoding 
+    on the receiving end will be robust up to one sample drop or one extra sample.
+
+    >>> mock = PulseEmulator(1000, 1) # record at 1000Hz, for 1 second
+    >>> pwm_transmitter = DiscretePwmProtocol(precision=6) #6 decimals rounding
+    >>> mock.start()
+    >>> pwm_transmitter.send_value(4.3, mock.sampling_rate, mock.on, mock.off) #doctest: +ELLIPSIS
+    (4.3, ...)
+    >>> pwm_transmitter.decode_values_from_signal(mock.get_signal()) #doctest: +ELLIPSIS
+    [(..., 4.3)]
+    """
+
+    NB_SAMPLES_DELIMITER = 7 # used to define begining and end of an encoding 
+    NB_SAMPLES_BIT0 = 5 
+    NB_SAMPLES_BIT1 = 2
+    NB_SAMPLES_SEP = 2
+    NB_BITS_PRECISION = 4 
+
+    SIG_THRESH_FACTOR = 0.5 # faction of signal maximum for thresholding
+    
+    def __init__(self, precision=6):
+        """
+        argument:
+            precision (int): number of decimal digits to keep (between 0 and 9)
+        """
+        assert(int(precision)==precision and precision <= 9 and precision >= 0)
+        self.precision = precision
+        
+    def value_to_bits(self, value):
+        """ 
+        Convert given value to a binary sequence using given precision. 
+    
+        arguments:
+            value (float): to convert
+    
+        output:
+            binary sequence as a string (without prefix '0b')
+        """
+        return bin(int(round(value * (10**self.precision))))[2:]
+
+    @staticmethod
+    def bits_to_value(bin_seq, precision):
+        """ 
+        Convert given binary sequence to a value using given precision. 
+    
+        arguments:
+            - bin_seq (string): binary sequence (without prefix '0b')
+            - precision (int): number of decimal digits to extract.
+                               Must be between 0 and 9.
+        """
+        return int('0b' + bin_seq, 2) / 10**precision
+
+    @classmethod
+    def decode_values_from_signal(cls, sig):
+        """ 
+        Decode all timestamps from given analog signal.
+    
+        Conversion from analog to binary values is done by thresholding 
+        above 50% of the max amplitude.
+        
+        argument:
+            - sig (np.array of float): pulse signal from which to extract 
+                                       encoded values
+    
+        output: list of tuple
+             Each entry of this list is:
+             (sample index in sig where coded value was found, 
+              decoded float value)        
+        """
+        
+        assert(sig.ndim==1)
+    
+        def mark_bins(bins, imark, padding=0, col_width=80):
+            # TODO: enable multiple markings
+            pad = ' ' * padding
+            bin_str = ''.join([str(int(v)) for v in bin_sig])
+            mark_str = ' ' * (imark) + '^' + ' ' * (len(bin_str)-imark)
+            from textwrap import wrap
+            return '\n'.join(['%s%s\n%s%s' % (pad, b, pad, m) \
+                              for b,m in zip(wrap(bin_str, col_width),
+                                             wrap(mark_str, col_width,
+                                                  replace_whitespace=False,
+                                                  drop_whitespace=False))])
+        
+        bin_sig = sig > (sig.max() * cls.SIG_THRESH_FACTOR)
+        interuptions = np.where(np.diff(bin_sig.astype(int))!=0)[0] + 1
+        chunks = np.split(bin_sig, interuptions)
+        bin_seq = []
+        values = []
+        seq_started = False
+        seq_start = None
+        for chunk_start, chunk in zip(np.concatenate(([0], interuptions)),
+                                      chunks):
+            if len(chunk) > 0:
+                logger.debug('Current chunk start (%d):\n%s',
+                             chunk_start, mark_bins(bin_sig, chunk_start))
+                logger.debug('Current chunk end (%d):\n%s',
+                             chunk_start+len(chunk)-1,
+                             mark_bins(bin_sig, chunk_start+len(chunk)-1))
+                #TODO: enable multiple marking in one single output
+                
+                if np.any(chunk) and abs(len(chunk) - cls.NB_SAMPLES_DELIMITER) < 2:
+                    if not seq_started: # next bits are to be decoded
+                        logger.debug('-> Chunk is start delimiter')
+                        seq_started = True
+                        precision_read = False
+                        precision = None
+                        seq_start = chunk_start
+                    else: # end of a current encoding
+                        logger.debug('-> Chunk is end delimiter')
+
+                        if not precision_read:
+                            raise DppDecodeError('Precision sequence not found')
+                        
+                        value = int(''.join(bin_seq),2) / 10**precision
+                        values.append((seq_start, value))
+                        logger.debug('Decoded value %f at position %d', value,
+                                     seq_start)
+                        
+                        bin_seq = []
+                        seq_started = False # wait for another sequence to start
+                        precision_read = False
+                        precision = None
+                elif seq_started:
+                    if np.any(chunk):
+                        if abs(len(chunk) - cls.NB_SAMPLES_BIT1) < 2:
+                            logger.debug('-> Chunk is BIT 1')
+                            bin_seq.append('1')
+                        elif abs(len(chunk) - cls.NB_SAMPLES_BIT0) < 2:
+                            logger.debug('-> Chunk is BIT 0')
+                            bin_seq.append('0')
+                        else:
+                            raise DppDecodeError('Error in chunk: %s' % \
+                                              ''.join([str(v) for v in chunk]))
+                    else:
+                        if abs(len(chunk) - cls.NB_SAMPLES_SEP) >= 2:
+                            raise DppDecodeError('Bad separator:\n%s' % \
+                                                 mark_bins(bin_sig, chunk_start))
+                    if not precision_read and \
+                       len(bin_seq) == cls.NB_BITS_PRECISION:
+                        precision = int(''.join(bin_seq),2)
+                        precision_read = True
+                        bin_seq = [] # reset list to accumulate bits for the
+                                     # encoded value
+                        
+        if seq_started:
+            raise DppDecodeError('Unterminated sequence:\n%s' % \
+                                 mark_bins(bin_sig, seq_start))
+        
+        return values
+        
+    def send_value(self, value, sampling_rate, on_func=None, off_func=None):
+        """
+        Transmit the given value using pulse width modulation. Pulse emission 
+        is done via the given functions on_func and off_func.
+        Transmission is time-critical so that the pulse sequence can be 
+        synchronized with value resolving. Indeed, value can be passed 
+        in a delayed manner via a callable (see argument documentation).
+
+        The function returns the overhead delay between the function call and
+        when the first encoded bit is transmitted.
+        
+        argument:
+            - value (float or callable): value to transmit. If it's a callable, 
+                                         it must return a single float value.
+            - sampling_rate (float): sampling rate of the receiving interface,
+                                     in Hz.
+            - on_func (callable): set the pulse on. Default prints "ON"
+            - off_func (callable): set the pulse off. Default prints "OFF"
+
+        output: tuple
+            (transmitted value,
+             delay in second (float) between the function call and the start 
+             of the encoded sequence transmission,
+             transmission time in second (float))
+        """
+        t_call = time.perf_counter()
+
+        if not callable(value):
+            get_value = lambda : value
+        else:
+            get_value = value
+            
+        if on_func is None:
+            on_func = lambda : print('ON')
+    
+        if off_func is None:
+            off_func = lambda : print('OFF')
+
+        bin_seq_precision = "{0:04b}".format(self.precision)
+        
+        dt = 1/sampling_rate
+    
+        # Insure there is some zero before starting the encoding sequence:
+        t_start_send = time.perf_counter()
+        off_func()
+        time.sleep(dt * DiscretePwmProtocol.NB_SAMPLES_SEP - \
+                   time.perf_counter() + t_start_send)
+        
+        tic = time.perf_counter()
+        # Value resolving is done right before the start of the delimiting sequence
+        value_tr = get_value()
+        delay = time.perf_counter() - t_call # Overhead time
+        on_func() # Pulse start for the delimiting sequence 
+        bin_seq_value = self.value_to_bits(value_tr) # Convert to binary 
+        time.sleep(dt * DiscretePwmProtocol.NB_SAMPLES_DELIMITER - \
+                   time.perf_counter() + tic)
+    
+        # Send a separator
+        tic = time.perf_counter()
+        off_func()
+        time.sleep(dt * DiscretePwmProtocol.NB_SAMPLES_SEP - \
+                   time.perf_counter() + tic)
+        
+        def send_bits(bits):
+            # Loop through all given bits and send pulses using width encoding
+            for b in bits:
+                tic = time.perf_counter()
+                if b == '1':
+                    on_func()
+                    time.sleep(dt * DiscretePwmProtocol.NB_SAMPLES_BIT1 - \
+                               time.perf_counter() + tic)
+                else:
+                    on_func()
+                    time.sleep(dt * DiscretePwmProtocol.NB_SAMPLES_BIT0 - \
+                               time.perf_counter() + tic)
+                tic = time.perf_counter()
+                off_func()
+                time.sleep(dt * DiscretePwmProtocol.NB_SAMPLES_SEP - \
+                           time.perf_counter() + tic)
+        # Send precision bits:
+        send_bits(bin_seq_precision)
+        # Send value bits:
+        send_bits(bin_seq_value)
+        
+        # Send the delimiting sequence to tag the end:
+        tic = time.perf_counter()
+        on_func()
+        time.sleep(dt * DiscretePwmProtocol.NB_SAMPLES_DELIMITER - \
+                   time.perf_counter() + tic)
+        tic = time.perf_counter()
+        off_func()
+        time.sleep(dt * DiscretePwmProtocol.NB_SAMPLES_SEP - \
+                   time.perf_counter() + tic)
+        
+        return value_tr, delay, time.perf_counter() - t_start_send
+    
+#### Tests ####
+
+class RecordTerminated(Exception): pass
+    
+class Recorder(Thread):
+    """ 
+    Emulate an analog signal recorder that makes periodic readings, for tests.
+    """
+    
+    def __init__(self, tracked, sampling_rate, max_duration):
+        """
+        arguments:
+            - tracked (list of float): number to track encapsulated in a list 
+                                       (list length=1)
+            - sampling_rate (float), in Hz
+            - max_duration (float), in sec
+        """
+        Thread.__init__(self)
+        self.tracked = tracked
+        self.sampling_rate = sampling_rate
+        self.record_pace = round(1/sampling_rate * 1e6) / 1e6 #round to microsec
+        self.buffer_size = int(round(max_duration / self.record_pace))
+        self.buffer = np.zeros(self.buffer_size, dtype=type(tracked[0]))
+        self.i_sample = 0
+
+        self.finished = False
+
+        self.thread_start_ts = None
+        self.record_start_ts = None
+
+    def get_record_start_delay(self):
+        """ 
+        Get delay between call of Thread.start() and recording of first value
+
+        Return None if not started.
+        """
+        if self.thread_start_ts is None or self.record_start_ts is None:
+            return None
+        return self.record_start_ts - self.thread_start_ts
+    
+    def record(self):
+        """ Record a single value """
+        if self.i_sample < self.buffer_size:
+            # print('recording:', self.tracked[0], 'i_sample:', self.i_sample)
+            self.buffer[self.i_sample] = self.tracked[0]
+            self.i_sample += 1
+        else:
+            raise RecordTerminated()
+        
+    def run(self):
+        """ 
+        Record loop, insuring no temporal drift and accounting for
+        overhead delay (see get_record_start_delay).
+        """
+        if not self.finished:
+            # Do first step taking into account thread starting delay
+            t0 = time.perf_counter()
+            self.record_start_ts = time.time()        
+            try:
+                self.record()
+            except RecordTerminated:
+                self.finished = True
+                return
+            time.sleep(self.record_pace - time.perf_counter() + t0 - \
+                       self.record_start_ts + self.thread_start_ts)
+            
+        while not self.finished:
+            t0 = time.perf_counter()
+            try:
+                self.record()
+            except RecordTerminated:
+                break
+            time.sleep(self.record_pace - time.perf_counter() + t0)
+
+        self.finished = True
+        
+    def start(self):
+        """ Override Thread.start """
+        self.thread_start_ts = time.time()
+        super().start()
+        
+    def stop(self):
+        """ Override Thread.stop """
+        self.finished = True
+        
+    def get_signal(self):
+        """ 
+        Return the recorded signal. 
+        Return only zeros when the recording has not been done.
+        """
+        return self.buffer
+
+class PulseEmulator(Recorder):
+    """ Pulse emulator holding a binary flag to represent pulses. """
+    
+    def __init__(self, sampling_rate, max_duration):
+        self.pulse_state = [0]
+        super().__init__(self.pulse_state, sampling_rate, max_duration)
+
+    def on(self):
+        self.pulse_state[0] = 1
+
+    def off(self):
+        self.pulse_state[0] = 0
---
+++ test/test_procotol.py
@@ -0,0 +1,81 @@
+
+import unittest
+import time
+
+import numpy as np
+
+from polos.protocol import DiscretePwmProtocol, Recorder
+
+class DiscretePwmProtocolTest(unittest.TestCase):
+    
+    def test_decode_isolated_signal(self):
+        bin_seq = '001' # 1
+        precision = 9
+        expected_value = 1*10**(-precision)
+        
+        sig = np.array([1,1,1,1,1,1,1,0,             # delimiter
+                        1,0,1,1,1,1,0,1,1,1,1,0,1,0,  # precision
+                        1,1,1,1,0,1,1,1,1,0,0,1,0,   # value
+                        1,1,1,1,1,1,1])              # delimiter
+        sig = sig + np.random.rand(*sig.shape) * 0.5
+        found = DiscretePwmProtocol.decode_values_from_signal(sig)
+        self.assertEqual(found[0][1], expected_value)
+        self.assertEqual(found[0][0], 0)
+
+    def test_decode_signal(self):
+        bin_seq = '101' # 5
+        precision = 0
+        expected_value = 5
+            
+        sig = np.array(
+            [0,0,0,1,0,                                       # unrelated
+             1,1,1,1,1,1,1,1,0,0,                             # delimiter
+             1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0, # precision
+             1,1,0,1,1,1,1,1,0,0,1,0,0,                       # value
+             1,1,1,1,1,1,0,                                   # delimiter
+             0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0])        # unrelated
+        sig = sig + np.random.rand(*sig.shape) * 0.5
+        found = DiscretePwmProtocol.decode_values_from_signal(sig)
+        self.assertEqual(found[0][1], expected_value)
+        self.assertEqual(found[0][0], 5)
+
+    def test_decode_timestamp(self):
+        current_time = time.time()
+        precision = 6 # microsecond precision
+        sender = DiscretePwmProtocol(precision=precision)
+        bin_seq = sender.value_to_bits(current_time)
+        decoded_value = DiscretePwmProtocol.bits_to_value(bin_seq, precision)
+        self.assertTrue(abs(decoded_value - current_time) < 10**(-precision))
+
+    def test_send_timestamp(self):
+        sampling_rate = 1000 # Hz
+        recording_max_duration = 0.5 # second
+        gpio_state = [0]
+        
+        def gpio_on():
+            # print('gpio on')
+            gpio_state[0] = 1
+    
+        def gpio_off():
+            # print('gpio off')
+            gpio_state[0] = 0
+    
+        recorder = Recorder(gpio_state, sampling_rate, recording_max_duration)
+        precision = 6 # microsecond precision
+        sender = DiscretePwmProtocol(precision=precision) 
+        
+        recorder.start()
+        onset, send_delay, tr_time = sender.send_value(time.time, sampling_rate,
+                                                       on_func=gpio_on,
+                                                       off_func=gpio_off)
+    
+        recorder.stop()
+            
+        found = sender.decode_values_from_signal(recorder.get_signal())
+        self.assertTrue(abs(found[0][1] - onset) < 10**(-precision))
+        delay_record_trigger = send_delay + recorder.get_record_start_delay()
+        self.assertTrue(found[0][0] <= np.ceil((delay_record_trigger) * sampling_rate))
+            
+if __name__ == "__main__":
+    # logger.setLevel(0) #TODO: add command option?
+    unittest.main()
