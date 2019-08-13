05b6ce593f9649ff7d9e1c4c9d6b15a33029bfd8
tvvincent <20100thomas@gmail.com>
2019-08-13 10:27:50 -0400

    Add global check for python v3 or higher

--- python/polos/__init__.py
+++ python/polos/__init__.py
@@ -1,5 +1,11 @@
+import sys
+if sys.version_info[0] < 3:
+    raise Exception('Python 3 or newer is required.')
+
 from ._polos import STATUS_ERROR, STATUS_OK, STATUS_WARNING, STATUS_LABELS
 from ._polos import get_rtc_battery_status, get_rtc_run_status, get_rtc_installation_status, get_rtc_dtoverlay_id
 from ._polos import is_fake_hwclock_removed, is_systemd_ntp_disabled, ref_time_server_is_ntp_running, ref_time_server_is_ntp_synced, is_ntp_queryable
 from ._polos import client_get_ntp_source_ip, client_is_ntp_running
 from ._polos import get_local_ip, print_status
+
+
